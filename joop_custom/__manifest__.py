64675ae441913c6ecdadf187a79fe261b3227b4e
diegolacunza1420 <diegolacunza1420@gmail.com>
2026-01-08 20:53:21 -0600

    fix xpath in res_partner

--- joop_custom/__manifest__.py
+++ joop_custom/__manifest__.py
@@ -8,7 +8,7 @@ Joop Solutions Custom
     'author': 'JJM',
     'email': 'admin@jjm.mx',
     'category': 'Custom Development',
-    'version': '18.0.0.1.0',
+    'version': '19.0.0.2.0',
     'depends': [
         'base',
         'purchase',
