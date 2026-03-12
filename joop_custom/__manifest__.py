4f9872066080d0fb84efc8cced7377a3aeaa1636
diegolacunza1420 <diegolacunza1420@gmail.com>
2026-03-12 08:49:03 -0600

    update joop_custom

--- joop_custom/__manifest__.py
+++ joop_custom/__manifest__.py
@@ -11,7 +11,7 @@ Joop Solutions Custom
     'author': 'JJM',
     'email': 'admin@jjm.mx',
     'category': 'Custom Development',
-    'version': '19.0.0.4.0',
+    'version': '19.0.0.5.0',
     'depends': [
         'base',
         'purchase',
